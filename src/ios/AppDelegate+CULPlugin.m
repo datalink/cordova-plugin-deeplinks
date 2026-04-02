42e0492aba2da0d35d79da4ac9c508c29cdfbe5d
Scott Davey <sdavey@datalink.com.au>
2026-04-02 18:06:21 +1100

    Update for cordova-ios@8 compat with xcode 26.4
    - added a lot of native logging to see exactly where the chain stops
    - added a runtime hook in CULPlugin.m so it no longer depends only on the fragile AppDelegate category path
    - changed the JS subscription handling so the callback is explicitly kept alive

--- src/ios/AppDelegate+CULPlugin.m
+++ src/ios/AppDelegate+CULPlugin.m
@@ -6,6 +6,7 @@
 
 #import "AppDelegate+CULPlugin.h"
 #import "CULPlugin.h"
+#import <Cordova/CDVViewController.h>
 
 /**
  *  Plugin name in config.xml
@@ -15,18 +16,30 @@ static NSString *const PLUGIN_NAME = @"UniversalLinks";
 @implementation AppDelegate (CULPlugin)
 
 - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
+    NSLog(@"[UniversalLinks] AppDelegate category continueUserActivity activityType=%@ url=%@", userActivity.activityType, userActivity.webpageURL);
+
     // ignore activities that are not for Universal Links
     if (![userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb] || userActivity.webpageURL == nil) {
+        NSLog(@"[UniversalLinks] AppDelegate category ignoring non-web activity");
         return NO;
     }
-    
+
+    id viewController = self.viewController;
+    if (![viewController isKindOfClass:[CDVViewController class]]) {
+        NSLog(@"[UniversalLinks] AppDelegate category viewController is not CDVViewController: %@", viewController);
+        return NO;
+    }
+
     // get instance of the plugin and let it handle the userActivity object
-    CULPlugin *plugin = [self.viewController getCommandInstance:PLUGIN_NAME];
+    CULPlugin *plugin = [(CDVViewController *)viewController getCommandInstance:PLUGIN_NAME];
+    NSLog(@"[UniversalLinks] AppDelegate category plugin instance=%@", plugin);
     if (plugin == nil) {
         return NO;
     }
-    
-    return [plugin handleUserActivity:userActivity];
+
+    BOOL handled = [plugin handleUserActivity:userActivity];
+    NSLog(@"[UniversalLinks] AppDelegate category handled=%@", handled ? @"YES" : @"NO");
+    return handled;
 }
 
 @end
--- src/ios/CULPlugin.m
+++ src/ios/CULPlugin.m
@@ -11,6 +11,10 @@
 #import "CDVPluginResult+CULPlugin.h"
 #import "CDVInvokedUrlCommand+CULPlugin.h"
 #import "CULConfigJsonParser.h"
+#import <Cordova/CDVViewController.h>
+#import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+#import <objc/message.h>
 
 @interface CULPlugin() {
     NSArray *_supportedHosts;
@@ -18,14 +22,96 @@
     NSMutableDictionary<NSString *, NSString *> *_subscribers;
 }
 
++ (void)installContinueUserActivityHookIfNeeded;
+
 @end
 
+static BOOL cul_continueUserActivityHookInstalled = NO;
+static IMP cul_originalContinueUserActivityImp = NULL;
+
+static BOOL CULHandleUserActivityForDelegate(id delegate, NSUserActivity *userActivity) {
+    if (userActivity == nil || ![userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb] || userActivity.webpageURL == nil) {
+        return NO;
+    }
+
+    id viewController = nil;
+    @try {
+        viewController = [delegate valueForKey:@"viewController"];
+    } @catch (NSException *exception) {
+        NSLog(@"[UniversalLinks] Failed to read app delegate viewController: %@", exception);
+        return NO;
+    }
+
+    if (![viewController isKindOfClass:[CDVViewController class]]) {
+        NSLog(@"[UniversalLinks] App delegate viewController is not CDVViewController: %@", viewController);
+        return NO;
+    }
+
+    CULPlugin *plugin = [(CDVViewController *)viewController getCommandInstance:@"UniversalLinks"];
+    NSLog(@"[UniversalLinks] Hook resolved plugin instance=%@", plugin);
+    if (plugin == nil) {
+        return NO;
+    }
+
+    BOOL handled = [plugin handleUserActivity:userActivity];
+    NSLog(@"[UniversalLinks] Hook handled user activity=%@", handled ? @"YES" : @"NO");
+    return handled;
+}
+
+static BOOL cul_swizzled_continueUserActivity(id selfObj, SEL _cmd, UIApplication *application, NSUserActivity *userActivity, void (^restorationHandler)(NSArray *)) {
+    NSLog(@"[UniversalLinks] Swizzled continueUserActivity activityType=%@ url=%@", userActivity.activityType, userActivity.webpageURL);
+
+    BOOL originalHandled = NO;
+    if (cul_originalContinueUserActivityImp != NULL) {
+        BOOL (*originalFunc)(id, SEL, UIApplication *, NSUserActivity *, void (^)(NSArray *)) = (BOOL (*)(id, SEL, UIApplication *, NSUserActivity *, void (^)(NSArray *)))cul_originalContinueUserActivityImp;
+        originalHandled = originalFunc(selfObj, _cmd, application, userActivity, restorationHandler);
+        NSLog(@"[UniversalLinks] Original continueUserActivity handled=%@", originalHandled ? @"YES" : @"NO");
+    }
+
+    BOOL pluginHandled = CULHandleUserActivityForDelegate(selfObj, userActivity);
+    return originalHandled || pluginHandled;
+}
+
 @implementation CULPlugin
+
++ (void)installContinueUserActivityHookIfNeeded {
+    if (cul_continueUserActivityHookInstalled) {
+        return;
+    }
+
+    id<UIApplicationDelegate> appDelegate = [UIApplication sharedApplication].delegate;
+    if (appDelegate == nil) {
+        NSLog(@"[UniversalLinks] Cannot install continueUserActivity hook because UIApplication delegate is nil");
+        return;
+    }
+
+    Class delegateClass = [appDelegate class];
+    SEL selector = @selector(application:continueUserActivity:restorationHandler:);
+    Method method = class_getInstanceMethod(delegateClass, selector);
+    IMP newImp = (IMP)cul_swizzled_continueUserActivity;
+    const char *types = "B@:@@?";
+
+    if (method != NULL) {
+        cul_originalContinueUserActivityImp = method_getImplementation(method);
+        method_setImplementation(method, newImp);
+        NSLog(@"[UniversalLinks] Installed swizzled continueUserActivity hook on %@ (existing implementation)", NSStringFromClass(delegateClass));
+    } else {
+        BOOL added = class_addMethod(delegateClass, selector, newImp, types);
+        NSLog(@"[UniversalLinks] Installed swizzled continueUserActivity hook on %@ (added=%@)", NSStringFromClass(delegateClass), added ? @"YES" : @"NO");
+        if (!added) {
+            return;
+        }
+    }
+
+    cul_continueUserActivityHookInstalled = YES;
+}
 
 #pragma mark Public API
 
 - (void)pluginInitialize {
     [self localInit];
+    [[self class] installContinueUserActivityHookIfNeeded];
+    NSLog(@"[UniversalLinks] pluginInitialize supportedHosts=%@", _supportedHosts);
     // Can be used for testing.
     // Just uncomment, close the app and reopen it. That will simulate application launch from the link.
 //    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onResume:) name:UIApplicationWillEnterForegroundNotification object:nil];
@@ -34,16 +120,17 @@
 //- (void)onResume:(NSNotification *)notification {
 //    NSUserActivity *activity = [[NSUserActivity alloc] initWithActivityType:NSUserActivityTypeBrowsingWeb];
 //    [activity setWebpageURL:[NSURL URLWithString:@"http://site2.com/news/page?q=1&v=2#myhash"]];
-//    
+//
 //    [self handleUserActivity:activity];
 //}
 
 - (void)handleOpenURL:(NSNotification*)notification {
+    NSLog(@"[UniversalLinks] handleOpenURL notification=%@", notification.object);
     id url = notification.object;
     if (![url isKindOfClass:[NSURL class]]) {
         return;
     }
-    
+
     CULHost *host = [self findHostByURL:url];
     if (host) {
         [self storeEventWithHost:host originalURL:url];
@@ -52,15 +139,18 @@
 
 - (BOOL)handleUserActivity:(NSUserActivity *)userActivity {
     [self localInit];
-    
+
     NSURL *launchURL = userActivity.webpageURL;
+    NSLog(@"[UniversalLinks] handleUserActivity url=%@ supportedHosts=%@", launchURL, _supportedHosts);
     CULHost *host = [self findHostByURL:launchURL];
     if (host == nil) {
+        NSLog(@"[UniversalLinks] handleUserActivity no matching host for url=%@", launchURL);
         return NO;
     }
-    
+
+    NSLog(@"[UniversalLinks] handleUserActivity matched host=%@", host.name);
     [self storeEventWithHost:host originalURL:launchURL];
-    
+
     return YES;
 }
 
@@ -68,7 +158,7 @@
     _supportedHosts = nil;
     _subscribers = nil;
     _storedEvent = nil;
-    
+
     [super onAppTerminate];
 }
 
@@ -78,9 +168,9 @@
     if (_supportedHosts) {
         return;
     }
-    
+
     _subscribers = [[NSMutableDictionary alloc] init];
-    
+
     // Get supported hosts from the config.xml or www/ul.json.
     // For now priority goes to json config.
     _supportedHosts = [self getSupportedHostsFromPreferences];
@@ -91,7 +181,7 @@
     if (jsonConfigPath) {
         return [CULConfigJsonParser parseConfig:jsonConfigPath];
     }
-    
+
     return [CULConfigXmlParser parse];
 }
 
@@ -103,6 +193,7 @@
  *  @param originalUrl launch url
  */
 - (void)storeEventWithHost:(CULHost *)host originalURL:(NSURL *)originalUrl {
+    NSLog(@"[UniversalLinks] storeEventWithHost host=%@ url=%@", host.name, originalUrl);
     _storedEvent = [CDVPluginResult resultWithHost:host originalURL:originalUrl];
     [self tryToConsumeEvent];
 }
@@ -115,6 +206,7 @@
  */
 - (CULHost *)findHostByURL:(NSURL *)launchURL {
     NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:launchURL resolvingAgainstBaseURL:YES];
+    NSLog(@"[UniversalLinks] findHostByURL host=%@ path=%@ query=%@", urlComponents.host, urlComponents.path, urlComponents.query);
     CULHost *host = nil;
     for (CULHost *supportedHost in _supportedHosts) {
         NSPredicate *pred = [NSPredicate predicateWithFormat:@"self LIKE[c] %@", supportedHost.name];
@@ -123,7 +215,7 @@
             break;
         }
     }
-    
+
     return host;
 }
 
@@ -131,18 +223,21 @@
 
 /**
  *  Try to send event to the web page.
- *  If there is a subscriber for the event - it will be consumed. 
+ *  If there is a subscriber for the event - it will be consumed.
  *  If not - it will stay until someone subscribes to it.
  */
 - (void)tryToConsumeEvent {
+    NSLog(@"[UniversalLinks] tryToConsumeEvent subscribers=%@ storedEvent=%@", _subscribers, _storedEvent);
     if (_subscribers.count == 0 || _storedEvent == nil) {
         return;
     }
-    
+
     NSString *storedEventName = [_storedEvent eventName];
     for (NSString *eventName in _subscribers) {
         if ([storedEventName isEqualToString:eventName]) {
             NSString *callbackID = _subscribers[eventName];
+            NSLog(@"[UniversalLinks] Consuming stored event %@ with callbackId=%@", storedEventName, callbackID);
+            [_storedEvent setKeepCallbackAsBool:YES];
             [self.commandDelegate sendPluginResult:_storedEvent callbackId:callbackID];
             _storedEvent = nil;
             break;
@@ -153,17 +248,23 @@
 #pragma mark Methods, available from JavaScript side
 
 - (void)jsSubscribeForEvent:(CDVInvokedUrlCommand *)command {
+    [self localInit];
     NSString *eventName = [command eventName];
+    NSLog(@"[UniversalLinks] jsSubscribeForEvent eventName=%@ callbackId=%@", eventName, command.callbackId);
     if (eventName.length == 0) {
         return;
     }
-    
+
     _subscribers[eventName] = command.callbackId;
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_NO_RESULT];
+    [pluginResult setKeepCallbackAsBool:YES];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     [self tryToConsumeEvent];
 }
 
 - (void)jsUnsubscribeFromEvent:(CDVInvokedUrlCommand *)command {
     NSString *eventName = [command eventName];
+    NSLog(@"[UniversalLinks] jsUnsubscribeFromEvent eventName=%@", eventName);
     if (eventName.length == 0) {
         return;
     }
